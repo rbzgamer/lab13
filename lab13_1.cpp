64c1b637411d4e29ed34ef57771064f4739c1243
Autoz <kuro123520@hotmail.com>
2021-01-22 16:28:49 +0700

    lab13_1 - success

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void Swap(T d[],int x,int y){
+	T temp = d[x];
+	d[x] = d[y];
+	d[y] = temp;
+}
 
+template <typename T>
+void insertionSort(T d[],int N){
+	int i,j;
+	T temp;
+	for(i = 1; i < N; i++){
+		temp = d[i];
+		j = i-1;
+		while(j >= 0 && d[j] < temp){
+			Swap(d,j+1,j);
+			j -= 1;
+		}
+		d[j+1] = temp;
+		cout << "Pass " << i << ":";
+		for(int j = 0; j < N; j++) cout << d[j] << " ";
+		cout << endl;
+	}
 }
 
 int main(){
@@ -17,3 +37,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+

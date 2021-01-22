8b5bc1fcd6b5bbb7f1a320f7bf91c0d4bde550fa
Autoz <kuro123520@hotmail.com>
2021-01-22 17:02:34 +0700

    lab13_2 - success

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,34 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double a[][N]){
+	for(int i = 0 ; i < N; i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j = 0; j < N; j++){
+		    cin >> a[i][j];
+	    }
+    }
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			b[i][j] = false;
+		}
+	}
+	for(int i = 1; i < N-1; i ++){
+		for(int j = 1; j < N-1; j++){
+			if(a[i][j] >= a[i-1][j] && a[i][j] >= a[i+1][j] && a[i][j] >= a[i][j+1] && a[i][j] >= a[i][j-1]) b[i][j] = true;
+		}
+	}
+}
+
+void showMatrix(const bool a[][N]){
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
